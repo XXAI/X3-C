5c094b6f786b11790bc101f63caffa49d5836583
akira.redwolf@gmail.com <akira.redwolf@gmail.com>
2017-03-14 12:07:04 -0600

    Se agregan correcciones al modulo de nuevo pedido.

--- src/app/farmacia/pedidos/nuevo/nuevo.component.ts
+++ src/app/farmacia/pedidos/nuevo/nuevo.component.ts
@@ -32,7 +32,7 @@ export class NuevoComponent implements OnInit {
   // # FIN SECCION
 
 
-
+/*
   // # SECCION: Resultados de búsqueda
   private ultimoTerminoBuscado = "";
   private terminosBusqueda = new Subject<string>();
@@ -45,7 +45,7 @@ export class NuevoComponent implements OnInit {
   private indicePaginasBusqueda:number[] = []
   // # FIN SECCION
 
-
+*/
 
   // # SECCION: Modal Insumos
   private mostrarModalInsumos = false;
@@ -61,6 +61,20 @@ export class NuevoComponent implements OnInit {
   private paginasTotales = 0;
   private indicePaginas:number[] = []
   // # FIN SECCION
+
+  // # SECCION: Resultados de filtro
+  private filtroActivado:boolean = false;
+  private listaInsumosFiltro:any[] = [];
+  private listaInsumosFiltroPaginada:any[] = [];
+  private paginaActualFiltro = 1;
+  private resultadosPorPaginaFiltro = 5;
+  private totalFiltro = 0;
+  private paginasTotalesFiltro = 0;
+  private indicePaginasFiltro:number[] = []
+
+  // # FIN SECCION
+
+
 
   constructor(private title:Title, private location:Location) { }
 
@@ -88,6 +102,32 @@ export class NuevoComponent implements OnInit {
         generico: "NAPROXENO",
         concentracion: "150mg",
         presentacion: "COMPRIMIDOS",
+        cantidad: null,
+        causes: true,
+        controlado: true,
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, esse. Inventore quasi dolore sapiente sequi, sunt natus fugiat quas eos, quia nostrum corporis voluptatem a distinctio! Adipisci, aperiam amet placeat."
+      }
+    );
+    this.listaInsumos.push(
+      {
+        clave: "010.000.0002.01",
+        tipo: "ME",
+        generico: "NAPROXENO",
+        concentracion: "500mg",
+        presentacion: "COMPRIMIDOS",
+        cantidad: null,
+        causes: true,
+        controlado: true,
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, esse. Inventore quasi dolore sapiente sequi, sunt natus fugiat quas eos, quia nostrum corporis voluptatem a distinctio! Adipisci, aperiam amet placeat."
+      }
+    );
+    this.listaInsumos.push(
+      {
+        clave: "010.000.0002.02",
+        tipo: "ME",
+        generico: "NAPROXENO",
+        concentracion: "500mg",
+        presentacion: "CAPSULAS",
         cantidad: null,
         causes: true,
         controlado: true,
@@ -589,6 +629,7 @@ export class NuevoComponent implements OnInit {
         descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, esse. Inventore quasi dolore sapiente sequi, sunt natus fugiat quas eos, quia nostrum corporis voluptatem a distinctio! Adipisci, aperiam amet placeat."
       }
     );
+    this.indexarLista();
     this.listar(1);
   }
   regresar(){
@@ -601,13 +642,55 @@ export class NuevoComponent implements OnInit {
   }
 
   // # SECCION Pedido
+  indexarLista(){
+    var contador = 1;
+    for(let i in this.listaInsumos){
+      this.listaInsumos[i].lote = contador++;
+    }
 
+    this.paginasTotales = Math.ceil(this.listaInsumos.length / this.resultadosPorPagina);
+    this.total = this.listaInsumos.length;
+
+    this.indicePaginas = [];
+    for(let i=0; i< this.paginasTotales; i++){
+      this.indicePaginas.push(i+1);
+    }
+  }
+  agregarItem(item:any = {}){
+    let auxPaginasTotales = this.paginasTotales;
+   
+    this.listaInsumos.push(
+      {
+        clave: "010.000.1327.00",
+        tipo: "ME",
+        generico: "CHUCHI",
+        concentracion: "150mg/ml",
+        presentacion: "SUSPENCIÓN",
+        cantidad: null,
+        causes: true,
+        controlado: false,
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, esse. Inventore quasi dolore sapiente sequi, sunt natus fugiat quas eos, quia nostrum corporis voluptatem a distinctio! Adipisci, aperiam amet placeat."
+      }
+    );
+    
+    this.indexarLista();
+
+    // El siguiente proceso es para cambiar de página automáticamente si se encuentra en la última.
+    if(this.listaInsumosPaginada.length == this.resultadosPorPagina 
+      && this.paginaActual == auxPaginasTotales
+      && !this.filtroActivado){
+      this.listar(this.paginaActual + 1);
+    } else{
+      this.listar(this.paginaActual);
+    }
+  }
   eliminarItem(item:any, index:number){
     var contador: number = 0;
     for(let i in this.listaInsumos){
       if(this.listaInsumos[i] === item){
         this.listaInsumosPaginada.splice(index, 1);  
         this.listaInsumos.splice(contador, 1);  
+        this.indexarLista();
         if(this.listaInsumosPaginada.length == 0){
           this.listar(this.paginaActual);
         }
@@ -622,27 +705,12 @@ export class NuevoComponent implements OnInit {
 
   // # SECCION: Paginación
   listar(pagina: number){
-    this.paginaActual = pagina;
-    this.paginasTotales = Math.ceil(this.listaInsumos.length / this.resultadosPorPagina);
-    this.total = this.listaInsumos.length;
-
-    this.indicePaginas = [];
-    for(let i=0; i< this.paginasTotales; i++){
-      this.indicePaginas.push(i+1);
-    }
+    this.paginaActual = pagina;    
 
     let inicio = (this.paginaActual - 1) * this.resultadosPorPagina;
     let fin = inicio + this.resultadosPorPagina;
     try {
       this.listaInsumosPaginada = this.listaInsumos.slice(inicio,fin);
-
-  
-      var lote = inicio + 1;
-      for( let item in this.listaInsumosPaginada ){
-        console.log(item)
-        this.listaInsumosPaginada[item].lote = lote;
-        lote++;
-      }
     } catch(e){
       this.listaInsumosPaginada = [];
     }
@@ -654,5 +722,53 @@ export class NuevoComponent implements OnInit {
   }
   paginaAnterior():void {
     this.listar(this.paginaActual-1);
+  }
+
+  // # - Filtros 
+  buscar(campos:string[],term:string){
+    if(term != ""){
+      this.filtroActivado = true;      
+    } else {
+      this.filtroActivado = false;
+      this.listaInsumosFiltro = [];
+      return;
+    }
+
+    this.listaInsumosFiltro = this.listaInsumos.filter((item)=> {   
+      var cadena = "";
+      for (let i in campos){
+        cadena += " " + item[campos[i]].toLowerCase();
+      }
+      return cadena.includes(term.toLowerCase())
+    });
+
+    this.paginaActualFiltro = 1;
+    this.listarFiltro(this.paginaActualFiltro);
+  }
+
+  listarFiltro(pagina: number){
+    this.paginaActualFiltro = pagina;
+    this.paginasTotalesFiltro = Math.ceil(this.listaInsumosFiltro.length / this.resultadosPorPaginaFiltro);
+    this.totalFiltro = this.listaInsumosFiltro.length;
+
+    this.indicePaginasFiltro = [];
+    for(let i=0; i< this.paginasTotalesFiltro; i++){
+      this.indicePaginasFiltro.push(i+1);
+    }
+
+    let inicio = (this.paginaActualFiltro - 1) * this.resultadosPorPaginaFiltro;
+    let fin = inicio + this.resultadosPorPaginaFiltro;
+    try {
+      this.listaInsumosFiltroPaginada = this.listaInsumosFiltro.slice(inicio,fin);
+    } catch(e){
+      this.listaInsumosFiltroPaginada = [];
+    }
+    
+  }
+  paginaSiguienteFiltro():void {
+    this.listarFiltro(this.paginaActualFiltro+1);
+  }
+  paginaAnteriorFiltro():void {
+    this.listarFiltro(this.paginaActualFiltro-1);
   }
 }
