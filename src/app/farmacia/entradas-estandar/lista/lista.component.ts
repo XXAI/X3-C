218a557c7c70e8090df18a67f3bcf1ceac12da79
deysi <deysukiz@gmail.com>
2017-08-17 16:38:45 -0500

    Entradas y salidas. Listado pdf y filtros.

--- src/app/farmacia/entradas-estandar/lista/lista.component.ts
+++ src/app/farmacia/entradas-estandar/lista/lista.component.ts
@@ -1,10 +1,122 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, NgZone } from '@angular/core';
+import  * as FileSaver    from 'file-saver';
 
 @Component({
   selector: 'salidas-recetas-lista',
   templateUrl: './lista.component.html'
 })
 
-export class ListaComponent{
+export class ListaComponent {
+
+  usuario;
+  fecha_desde = '';
+  fecha_hasta = '';
+  turno = '';
+  servicio = '';
+  recibe = '';
+  dato;
+
+  // # SECCION: Reportes
+  pdfworker: Worker;
+  cargandoPdf = false;
+  // # FIN SECCION
+
+  @ViewChildren('tr') tr;
+  @ViewChildren('sr') sr;
+
+  constructor(
+    private _ngZone: NgZone) { }
+
+  ngOnInit() {
+      this.usuario = JSON.parse(localStorage.getItem('usuario'));
+
+    // Inicializamos el objeto para los reportes con web Webworkers
+    this.pdfworker = new Worker('web-workers/farmacia/movimientos/lista-entradas.js');
+
+    // Este es un hack para poder usar variables del componente dentro de una funcion del worker
+    let self = this;
+    let $ngZone = this._ngZone;
+
+    this.pdfworker.onmessage = function( evt ) {
+      // Esto es un hack porque estamos fuera de contexto dentro del worker
+      // Y se usa esto para actualizar alginas variables
+      $ngZone.run(() => {
+         self.cargandoPdf = false;
+      });
+
+      FileSaver.saveAs( self.base64ToBlob( evt.data.base64, 'application/pdf' ), evt.data.fileName );
+      // open( 'data:application/pdf;base64,' + evt.data.base64 ); // Popup PDF
+    };
+  }
+
+  export_pdf(){
+    // crear el contenedor para guardar el elemento a imprimir
+    var titulo = 'Entrada Estandar';
+    var turno = this.tr.first.nativeElement.options;
+    var servicio = this.sr.first.nativeElement.options;
+    turno = turno[turno.selectedIndex].text;
+    servicio = servicio[servicio.selectedIndex].text;
+    var exportData = "<table><tr><th colspan='7'><h1>" + titulo 
+    +"</h1></th></tr><tr><th>Desde: "+this.fecha_desde+"</th><th>Hasta: "+this.fecha_hasta+"</th>"
+    +"</th><th>Recibe: "+this.recibe+"</th></tr><tr><th colspan='7'></th></tr></table>";
+
+    var exportData = exportData + document.getElementById("exportable").innerHTML;
+
+    exportData = '<html lang="es">' + ' <head>' + ' <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' 
+    + ' <meta name="charset" content="UTF-8">' + ' <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">' 
+    + ' <meta name="apple-mobile-web-app-capable" content="yes">' + ' <title>PDF</title> <meta name="viewport" content="initial-scale=1" />' 
+    + ' <style>html { font-size: .9em;} body{font-size: .9em;} select::-ms-expand {display: none;}</style>' + ' </head>' + ' <body>'  + exportData + ' </body>' + ' </html>';
+   
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute("id", 'printf');
+    iframe.setAttribute('style', 'display:none');
+    document.body.appendChild(iframe);
+
+    var mywindow = <HTMLSelectElement>document.getElementById('printf');
+    mywindow.contentWindow.document.write(exportData);
+    setTimeout(function () {
+      // lanzar la sentencia imprimir
+      mywindow.contentWindow.print();
+    }, 500);
+    setTimeout(function () {
+      // remover el contenedor de impresión
+      document.body.removeChild(iframe);
+    }, 2000);
+  }
+  export_excel(){
+    var titulo = 'Entrada Estandar';
+    var turno = this.tr.first.nativeElement.options;
+    var servicio = this.sr.first.nativeElement.options;
+    turno = turno[turno.selectedIndex].text;
+    servicio = servicio[servicio.selectedIndex].text;
+    var exportData = '<table><tr><th colspan=\'7\'><h1>' + titulo 
+    +'</h1></th></tr><tr><th>Desde: '+this.fecha_desde+'</th><th>Hasta: '+this.fecha_hasta+'</th>'
+    +'</th><th>Recibe: '+this.recibe+'</th></tr><tr><th colspan=\'7\'></th></tr></table>';
+
+    exportData += document.getElementById('exportable').innerHTML;
+    var blob = new Blob([exportData], { type: 'text/comma-separated-values;charset=utf-8' });
+    saveAs(blob,  'entrada_estandar.xls');
+  }
   
+  imprimir() {
+    try {
+      this.cargandoPdf = true;
+      let entrada_imprimir = {
+        lista: this.dato,
+        usuario: this.usuario
+      };
+      this.pdfworker.postMessage(JSON.stringify(entrada_imprimir));
+    } catch (e) {
+      this.cargandoPdf = false;
+    }
+  }
+
+  base64ToBlob( base64, type ) {
+      let bytes = atob( base64 ), len = bytes.length;
+      let buffer = new ArrayBuffer( len ), view = new Uint8Array( buffer );
+      for ( let i = 0 ; i < len ; i++ ) {
+        view[i] = bytes.charCodeAt(i) & 0xff;
+      }
+      return new Blob( [ buffer ], { type: type } );
+  }
 }
--- src/web-workers/farmacia/movimientos/lista-salidas.js
+++ src/web-workers/farmacia/movimientos/lista-salidas.js
@@ -5,7 +5,6 @@ importScripts('../../../scripts/pdfmake.min.js', '../../../scripts/vfs_fonts.js'
 (function() {
     'use strict';
 
-
     onmessage = function(evt) {
         let data = JSON.parse(evt.data)
         pdf(data);
@@ -33,41 +32,17 @@ importScripts('../../../scripts/pdfmake.min.js', '../../../scripts/vfs_fonts.js'
                         [{ text: 'SIAL', style: 'titulo', colSpan: 7, alignment: 'center' },
                             {}, {}, {}, {}, {}, {}
                         ],
-                        [{ text: 'SALIDA ESTANDAR', style: 'tableHeaderTop', colSpan: 7, alignment: 'center' },
+                        [{ text: 'SALIDAS DE ALMACEN', style: 'tableHeaderTop', colSpan: 7, alignment: 'center' },
                             {}, {}, {}, {}, {}, {}
                         ],
                         [
-                            { text: 'FOLIO', style: 'tableHeaderVerde', colSpan: 2, alignment: 'right' },
-                            {},
-                            { text: data.datos.id, style: 'tableHeader', colSpan: 3, alignment: 'left' }, {}, {},
-                            { text: 'ALMACÉN', style: 'tableHeaderVerde', alignment: 'right' },
-                            { text: data.usuario.almacen_activo.nombre, style: 'tableHeader', alignment: 'left' }
-                        ],
-                        [
-                            { text: 'ENTREGÓ', style: 'tableHeaderVerde', colSpan: 2, alignment: 'right' },
-                            {},
-                            { text: data.usuario.nombre +' '+ data.usuario.apellidos, style: 'tableHeader', colSpan: 3, alignment: 'left' }, {}, {},
-                            { text: 'FECHA DE CREACION', style: 'tableHeaderVerde', alignment: 'right' },
-                            { text: data.datos.fecha_movimiento, style: 'tableHeader', alignment: 'left' }
-                        ],
-                        [
-                            { text: 'CLUES', style: 'tableHeaderVerde', colSpan: 2, alignment: 'right' },
-                            {},
-                            { text: data.usuario.clues_activa.clues, style: 'tableHeader', colSpan: 3, alignment: 'left' }, {}, {},
-                            { text: 'NOMBRE DE CLUES', style: 'tableHeaderVerde', alignment: 'right' },
-                            { text: data.usuario.clues_activa.nombre, style: 'tableHeader', alignment: 'left' }
-                        ],
-                        [{ text: ' ', style: 'celdaEspacio', colSpan: 7, alignment: 'center' },
-                            {}, {}, {}, {}, {}, {}
-                        ],
-                        [
-                            { text: 'CLAVE', style: 'tableHeaderVerde', alignment: 'center' },
-                            { text: 'NOMBRE', style: 'tableHeaderVerde', alignment: 'center' },
-                            { text: 'NO. DE LOTE', style: 'tableHeaderVerde', alignment: 'center' },
-                            { text: 'FECHA DE CADUCIDAD', style: 'tableHeaderVerde', alignment: 'center' },
-                            { text: 'CODIGO DE BARRAS', style: 'tableHeaderVerde', alignment: 'center' },
-                            { text: 'CANTIDAD SURTIDA (Normal)', style: 'tableHeaderVerde', alignment: 'center' },
-                            { text: 'CANTIDAD SURTIDA (Unidosis)', style: 'tableHeaderVerde', alignment: 'center' },
+                            { text: 'FECHA', style: 'tableHeaderVerde', alignment: 'center' },
+                            { text: 'FOLIO', style: 'tableHeaderVerde', alignment: 'center' },
+                            { text: 'SERVICIO', style: 'tableHeaderVerde', alignment: 'center' },
+                            { text: 'TURNO', style: 'tableHeaderVerde', alignment: 'center' },
+                            { text: 'RECIBE', style: 'tableHeaderVerde', alignment: 'center' },
+                            { text: 'ENTREGÓ', style: 'tableHeaderVerde', alignment: 'center' },
+                            { text: 'CAPTURADO', style: 'tableHeaderVerde', alignment: 'center' },
                         ]
                         //Body -> insumos
                     ]
@@ -179,25 +154,19 @@ importScripts('../../../scripts/pdfmake.min.js', '../../../scripts/vfs_fonts.js'
 
         var suma_total_insumos = 0;
 
-
         for (var i in data.lista) {
-            var insumo = data.lista[i];
-
-            for (var j in insumo.lotes) {
-                var lote = insumo.lotes[j];
+            var movimiento = data.lista[i];
+            
                 dd.content[0].table.body.push([
-                    { text: lote.clave_insumo_medico, style: 'tableRow', alignment: 'center' },
-                    { text: insumo.detalles.descripcion, style: 'tableRow', alignment: 'center' },
-                    { text: lote.lote, style: 'tableRow', alignment: 'center' },
-                    { text: lote.fecha_caducidad, style: 'tableRow', alignment: 'center' },
-                    { text: lote.codigo_barras, style: 'tableRow', alignment: 'center' },
-                    { text: lote.modo_salida == 'N' ? lote.cantidad : "--", style: 'tableRow', alignment: 'center' },
-                    { text: lote.modo_salida == 'U' ? lote.cantidad : "--", style: 'tableRow', alignment: 'center' }
+                    { text: movimiento.fecha_movimiento ? movimiento.fecha_movimiento : 'No disponible' , style: 'tableRow', alignment: 'center' },
+                    { text: movimiento.id ? movimiento.id : 'No disponible', style: 'tableRow', alignment: 'center' },
+                    { text: movimiento.movimiento_metadato == null ? 'No disponible' : movimiento.movimiento_metadato.servicio.nombre, style: 'tableRow', alignment: 'center' },
+                    { text: movimiento.movimiento_metadato == null ? 'No disponible' : movimiento.movimiento_metadato.turno.nombre, style: 'tableRow', alignment: 'center' },
+                    { text: movimiento.movimiento_metadato == null ? 'No disponible' :  movimiento.movimiento_metadato.persona_recibe, style: 'tableRow', alignment: 'center' },
+                    { text: movimiento.movimiento_usuario == null ? 'No disponible' : movimiento.movimiento_usuario.nombre + ' ' + movimiento.movimiento_usuario.apellidos , style: 'tableRow', alignment: 'center' },
+                    { text: movimiento.created_at ? movimiento.created_at : 'No disponible', style: 'tableRow', alignment: 'center' }
                 ]);
-            }
-
         }
-
 
         dd.content[0].table.body.push(
             // Footer
@@ -211,15 +180,7 @@ importScripts('../../../scripts/pdfmake.min.js', '../../../scripts/vfs_fonts.js'
                 table: {
                     widths: ['*', '*'],
                     body: [
-                        [
-                            { text: '\n\n\n\n'+ data.datos.movimiento_metadato.persona_recibe, rowSpan: 2, style: 'tableRow' }, 
-                            { text: "Observaciones", style: 'text' }
-                        ],
-                        [
-                            '', 
-                            { text: '\n' + data.datos.observaciones, rowSpan: 2, alignment: 'justify' }
-                        ],
-                        ['Persona que recibe ', '']
+                        ['', '']
                     ],
                 },
                 layout: {
@@ -244,7 +205,7 @@ importScripts('../../../scripts/pdfmake.min.js', '../../../scripts/vfs_fonts.js'
         );
 
         pdfMake.createPdf(dd).getBase64(function(base64) {
-            postMessage({ fileName: 'Salida' + data.datos.id + '.pdf', base64: base64 });
+            postMessage({ fileName: 'Salidas_estandar.pdf', base64: base64 });
         });
     }
 
