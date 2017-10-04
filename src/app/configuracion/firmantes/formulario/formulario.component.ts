46db6cbae95662c002e9893edc4b16eb1e51440d
Mario Alberto Villatoro <ing.mario.villatoro@gmail.com>
2017-10-04 13:10:13 -0500

    Actualizacion api de avance y configuracion

--- src/app/configuracion/firmantes/formulario/formulario.component.ts
+++ src/app/configuracion/firmantes/formulario/formulario.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, NgZone } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Location}           from '@angular/common';
 import { ActivatedRoute, Params, Router } from '@angular/router'
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
@@ -22,11 +22,19 @@ export class FormularioComponent implements OnInit {
 
   firmantes: FormGroup;
   cargando:boolean = false;
+  cargando_personal:boolean = false;
   showFirmante:boolean = false;
+  agregando_personal:boolean = false;
+  personal_seleccionado:number = 0;
+  buscarPersonal:string = "";
 
-    private paginaActual = 1;
+  private paginaActual = 1;
 	resultadosPorPagina = 25;
 	total = 0;
+  personal:any[] = [];
+  pedidos:any[] = [];
+  pedidos_check:any[] = [];
+
 	private paginasTotales = 0;
 	private indicePaginas:number[] = [];
 	// # SECCION: Resultados de búsqueda
@@ -35,7 +43,7 @@ export class FormularioComponent implements OnInit {
 	private resultadosBusqueda: any;
 	busquedaActivada:boolean = false;
 	private paginaActualBusqueda = 1;
-	resultadosPorPaginaBusqueda = 25;
+	resultadosPorPaginaBusqueda = 10;
 	totalBusqueda = 0;
 	private paginasTotalesBusqueda = 0;
 	private indicePaginasBusqueda:number[] = [];
@@ -56,9 +64,15 @@ export class FormularioComponent implements OnInit {
 
   ngOnInit() {
   	this.firmantes = this.fb.group({
-        firma_director: ['', [Validators.required]],
-        firma_almacen: ['', [Validators.required]]
+        firma_director:     ['', []],
+        id_firma_director:  ['', []],
+        firma_almacen: ['', []],
+        id_firma_almacen:  ['', []],
+        pedidos: this.fb.array([])
     });
+
+    this.listar_pedido();
+    
 
     this.title.setTitle('Firmantes / Configuración');
 
@@ -71,15 +85,17 @@ export class FormularioComponent implements OnInit {
 
       this.ultimoTerminoBuscado = term;
       this.paginaActualBusqueda = 1;
-      this.cargando = true; 	  
+      this.cargando_personal = true; 	  
 
- 	  return false ? this.firmanteService.buscar_personal(term, this.paginaActualBusqueda, this.resultadosPorPaginaBusqueda) : Observable.of<any>({data:[]})
+
+       //return false ? this.firmanteService.buscar_personal(term, this.paginaActualBusqueda, this.resultadosPorPaginaBusqueda) : Observable.of<any>({data:[]})
+ 	    return this.firmanteService.buscar_personal(term, this.paginaActualBusqueda, this.resultadosPorPaginaBusqueda)
     }
       
     
     ).catch( function handleError(error){ 
      
-      self.cargando = false;      
+      self.cargando_personal = false;      
       self.mensajeError.mostrar = true;
       self.ultimaPeticion = function(){self.listarBusqueda(self.ultimoTerminoBuscado,self.paginaActualBusqueda);};//OJO
       try {
@@ -103,8 +119,8 @@ export class FormularioComponent implements OnInit {
 
     busquedaSubject.subscribe(
       resultado => {
-        /*this.cargando = false;
-        this.resultadosBusqueda = resultado.data as Tema[];
+        this.cargando_personal = false;
+        this.personal = resultado.data;
         this.totalBusqueda = resultado.total | 0;
         this.paginasTotalesBusqueda = Math.ceil(this.totalBusqueda / this.resultadosPorPaginaBusqueda);
 
@@ -112,44 +128,45 @@ export class FormularioComponent implements OnInit {
         for(let i=0; i< this.paginasTotalesBusqueda; i++){
           this.indicePaginasBusqueda.push(i+1);
         }
-        
-        console.log("Búsqueda cargada.");*/
+       
       }
 
     );
   }
 
-   buscar_firmante():void
+   buscar_firmante(id:number, text:string):void
    {
+      this.personal = [];
   		this.showFirmante = !this.showFirmante;
+      this.personal_seleccionado = id; 
+      this.buscarPersonal = text;
    }
 
-   buscar_personal():void
+   buscar_personal(term:string):void
    {
-  		
+      this.terminosBusqueda.next(term);
    }
 
    listar(pagina:number): void {
     this.paginaActual = pagina;
     
-    /*this.cargando = true;
-    this.avanceService.lista(pagina,this.resultadosPorPagina).subscribe(
+    this.cargando_personal = true;
+    this.firmanteService.lista(pagina,this.resultadosPorPagina).subscribe(
         resultado => {
 
-          this.cargando = false;
-          this.temas = resultado.data as Tema[];
+          this.cargando_personal = false;
+          this.personal = resultado.data;
+          this.totalBusqueda = resultado.total | 0;
+          this.paginasTotalesBusqueda = Math.ceil(this.totalBusqueda / this.resultadosPorPaginaBusqueda);
 
-          this.total = resultado.total | 0;
-          this.paginasTotales = Math.ceil(this.total / this.resultadosPorPagina);
-
-          this.indicePaginas = [];
-          for(let i=0; i< this.paginasTotales; i++){
-            this.indicePaginas.push(i+1);
+          this.indicePaginasBusqueda = [];
+          for(let i=0; i< this.paginasTotalesBusqueda; i++){
+            this.indicePaginasBusqueda.push(i+1);
           }
           
         },
         error => {
-          this.cargando = false;
+          this.cargando_personal = false;
           this.mensajeError.mostrar = true;
           this.ultimaPeticion = this.listar;
           try {
@@ -168,15 +185,226 @@ export class FormularioComponent implements OnInit {
           }
 
         }
-      );*/
+      );
+    }
+
+    obtener_firmantes(): void {
+      this.cargando = true;
+      this.firmanteService.firmantes().subscribe(
+        resultado => {
+
+         this.cargando = false;
+          if(resultado['unidad']['director'])
+          {
+              this.firmantes.patchValue({firma_director: resultado['unidad']['director']['nombre'], id_firma_director: resultado['unidad']['director']['id']});
+          }
+          if(resultado['almacen']['encargado'])
+          {
+            this.firmantes.patchValue({firma_almacen: resultado['almacen']['encargado']['nombre'], id_firma_almacen: resultado['almacen']['encargado']['id']});
+          }
+        },
+        error => {
+          this.cargando = false;
+          try {
+            let e = error.json();
+            if (error.status == 401 ){
+              this.mensajeError.texto = "No tiene permiso para hacer esta operación.";
+            }
+          } catch(e){
+            console.log("No se puede interpretar el error");
+            
+            if (error.status == 500 ){
+              this.mensajeError.texto = "500 (Error interno del servidor)";
+            } else {
+              this.mensajeError.texto = "No se puede interpretar el error. Por favor contacte con soporte técnico si esto vuelve a ocurrir.";
+            }            
+          }
+
+        }
+      );
+    }
+
+    listar_pedido(): void {
+    
+      this.cargando = true;
+      this.firmanteService.lista_pedido().subscribe(
+          resultado => {
+
+            this.pedidos = resultado;
+            this.obtener_firmantes();
+            
+          },
+          error => {
+            this.cargando = false;
+            this.mensajeError.mostrar = true;
+            try {
+              let e = error.json();
+              if (error.status == 401 ){
+                this.mensajeError.texto = "No tiene permiso para hacer esta operación.";
+              }
+            } catch(e){
+              console.log("No se puede interpretar el error");
+              
+              if (error.status == 500 ){
+                this.mensajeError.texto = "500 (Error interno del servidor)";
+              } else {
+                this.mensajeError.texto = "No se puede interpretar el error. Por favor contacte con soporte técnico si esto vuelve a ocurrir.";
+              }            
+            }
+
+          }
+        );
     }
 
    listarBusqueda(term:string ,pagina:number): void {
-   }
+      this.paginaActualBusqueda = pagina;
+      console.log("Cargando búsqueda.");
+     
+      this.cargando_personal = true;
+      this.firmanteService.buscar(term, pagina, this.resultadosPorPaginaBusqueda).subscribe(
+          resultado => {
+            this.cargando_personal = false;
+            this.personal = resultado.data;
+            this.totalBusqueda = resultado.total | 0;
+            this.paginasTotalesBusqueda = Math.ceil(this.totalBusqueda / this.resultadosPorPaginaBusqueda);
+
+            this.indicePaginasBusqueda = [];
+            for(let i=0; i< this.paginasTotalesBusqueda; i++){
+              this.indicePaginasBusqueda.push(i+1);
+            }
+            
+          },
+          error => {
+            this.cargando_personal = false;
+            this.mensajeError.mostrar = true;
+            this.ultimaPeticion = function(){this.listarBusqueda(term,pagina);};
+            try {
+              let e = error.json();
+              if (error.status == 401 ){
+                this.mensajeError.texto = "No tiene permiso para hacer esta operación.";
+              }
+            } catch(e){
+              console.log("No se puede interpretar el error");
+              
+              if (error.status == 500 ){
+                this.mensajeError.texto = "500 (Error interno del servidor)";
+              } else {
+                this.mensajeError.texto = "No se puede interpretar el error. Por favor contacte con soporte técnico si esto vuelve a ocurrir.";
+              }            
+            }
+
+          }
+        );
+    }
+
+    AgregarPersonal(personal:string):void
+    {
+      this.agregando_personal = true;
+      if(this.personal_seleccionado == 1)
+      {
+        this.firmantes.patchValue({firma_director:personal, id_firma_director:0});
+      }
+      else if(this.personal_seleccionado == 2)
+      {
+        this.firmantes.patchValue({firma_almacen:personal, id_firma_almacen:0});
+      }
+      this.showFirmante = false;
+      this.asignar_firmantes();
+    }
+
+    asignar_firmantes(): void
+    {
+      if(prompt("Para actualizar los firmastes de esta unidad por favor escriba ACTUALIZAR ") == "ACTUALIZAR")
+      {
+        this.firmanteService.actualizar_firmantes(this.firmantes.value).subscribe(
+          resultado => {
+            this.agregando_personal = false;
+            this.mensajeExito = new Mensaje(true);
+            this.mensajeExito.mostrar = true;             
+            this.mensajeExito.texto = "Se ha asignado correctamente los firmantes";
+            this.reset_pedidos();
+            this.listar_pedido();
+          },
+          error => {
+            this.mensajeError = new Mensaje(true);
+           this.agregando_personal = false;
+            try {
+              let e = error.json();
+              if (error.status == 401 ){
+                this.mensajeError.texto = "No tiene permiso para hacer esta operación.";
+              }
+            } catch(e){
+              console.log("No se puede interpretar el error");
+              
+              if (error.status == 500 ){
+                this.mensajeError.texto = "500 (Error interno del servidor)";
+              } else {
+                this.mensajeError.texto = "No se puede interpretar el error. Por favor contacte con soporte técnico si esto vuelve a ocurrir.";
+              }            
+            }
+            this.mensajeError.texto = "Ha ocurrido un error al asignar los firmantes";
+            this.mensajeError.mostrar = true; 
+
+          }
+        );
+      }
+    }
 
   	paginaSiguiente():void {
 	    this.listar(this.paginaActual+1);
 	}
+
+  seleccionarPersonal(obj:any):void
+  {
+    if(this.personal_seleccionado == 1)
+    {
+      this.firmantes.patchValue({firma_director:obj.nombre, id_firma_director:obj.id});
+    }
+    else if(this.personal_seleccionado == 2)
+    {
+      this.firmantes.patchValue({firma_almacen:obj.nombre, id_firma_almacen:obj.id});
+    }
+
+    this.showFirmante = false;
+
+  }
+
+  eliminar_firmante(id:number):void
+  {
+      if(id == 1)
+      {
+          this.firmantes.patchValue({firma_director:'', id_firma_director:'NULL'});
+      }
+      if(id == 2)
+      {
+          this.firmantes.patchValue({firma_almacen:'', id_firma_almacen:'NULL'});
+      }
+  }
+
+  reset_pedidos():void
+  {
+      const pedidosFormArray = <FormArray>this.firmantes.controls.pedidos;
+      console.log(pedidosFormArray.length);
+      for(var i = (pedidosFormArray.length-1); i>=0; i--)
+        pedidosFormArray.removeAt(i);
+    
+  }
+
+  updateCheckedOptions(item:string, isChecked: any): void
+  {
+    const pedidosFormArray = <FormArray>this.firmantes.controls.pedidos;
+
+      if(isChecked.target.checked) {
+        pedidosFormArray.push(new FormControl(item));
+      } else {
+        let index = pedidosFormArray.controls.findIndex(x => x.value == item)
+        pedidosFormArray.reset();
+      }  
+
+      console.log(this.firmantes);
+  }
+
+
 	paginaAnterior():void {
 	    this.listar(this.paginaActual-1);
 	}
