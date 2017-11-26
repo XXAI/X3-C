9977d1f759628acf824c240d2baf31c8684e331c
akira.redwolf@gmail.com <akira.redwolf@gmail.com>
2017-11-26 12:57:57 -0600

    Se agrega capacidad de editar perfil

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { AuthService } from 'app/auth.service';
 import { BloquearPantallaService }     from '../bloquear-pantalla/bloquear-pantalla.service';
 import { CambiarEntornoService }     from '../perfil/cambiar-entorno.service';
+import { EditarPerfilService }     from '../perfil/editar-perfil.service';
 
 @Component({
   selector: 'app-perfil',
@@ -16,18 +17,66 @@ export class PerfilComponent implements OnInit {
   usuario: any = {};
   mostrarCambiarEntorno:boolean = false;
   
+  edicionPerfil:boolean = false;
+
+  avatars = [
+    { value:"avatar-circled-user-male", title:"Hombre"},
+    { value:"avatar-circled-user-female", title:"Mujer"},
+    { value:"avatar-circled-user-male-skin-type-6", title:"Hombre piel morena"},
+    { value:"avatar-circled-user-female-skin-type-6", title:"Mujer piel morena"},
+    { value:"avatar-circled-collaborator-male", title:"Colaborador hombre"},
+    { value:"avatar-nurse", title:"Enfermera"},
+    { value:"avatar-circled-doctor-male", title:"Doctor"},
+    { value:"avatar-circled-doctor-female", title:"Doctora"},
+    { value:"avatar-cat", title:"Gato"}
+  ]
+  errores = {
+    nombre: null,
+    apellidos: null,
+    passwordAnterior:null,
+    passwordNuevo:null
+  }
+  nuevoPerfil: any = {
+    id:"",
+    avatar:"",
+    nombre:"",
+    apellidos:"",
+    cambiarPassword:false,
+    passwordAnterior:"",
+    passwordNuevo:"",
+    passwordNuevoConfirmacion:""
+  }
+
   constructor(
     private router: Router,
     private authService:AuthService,
     private bloquearPantallaService: BloquearPantallaService,
-    private cambiarEntornoService: CambiarEntornoService
+    private cambiarEntornoService: CambiarEntornoService,
+    private editarPerfilService: EditarPerfilService
   ) { }
 
   ngOnInit() {
     this.usuario = JSON.parse(localStorage.getItem("usuario"));
+
+    this.nuevoPerfil.id = this.usuario.id;
+    this.nuevoPerfil.avatar = this.usuario.avatar;
+    this.nuevoPerfil.nombre = this.usuario.nombre;
+    this.nuevoPerfil.apellidos = this.usuario.apellidos;
+    if(this.usuario.su == 1){
+      this.avatars.push({value: 'avatar-circled-root', title: "Super administrador"})
+    }
   }
 
   toggle() {
+    if(this.enviandoDatosPerfil){
+      return;
+    }
+    this.edicionPerfil = false;
+    this.nuevoPerfil.id = this.usuario.id;
+    this.nuevoPerfil.avatar = this.usuario.avatar;
+    this.nuevoPerfil.nombre = this.usuario.nombre;
+    this.nuevoPerfil.apellidos = this.usuario.apellidos;
+
     this.mostrar = !this.mostrar;
     if(this.mostrar){
       this.usuario = JSON.parse(localStorage.getItem("usuario"));
@@ -82,5 +131,72 @@ export class PerfilComponent implements OnInit {
     localStorage.setItem('usuario', JSON.stringify(this.usuario));
     this.mostrarCambiarEntorno = false;
     this.cambiarEntornoService.cambiarEntorno();
+  }
+
+  enviandoDatosPerfil:boolean = false;
+  mostrarMensajeExitoso:boolean = false;
+  mensajeError:string = '';
+  editarPerfil(){
+    this.enviandoDatosPerfil = true;
+    
+    var payload = this.nuevoPerfil;
+    if(!payload.cambiarPassword){
+      payload.cambiarPassword = null;
+    }
+
+    this.errores = {
+      nombre: null,
+      apellidos: null,
+      passwordAnterior:null,
+      passwordNuevo:null
+    }
+    this.mensajeError = '';
+    this.editarPerfilService.editar(this.usuario.id, payload).subscribe(
+      respuesta => {
+        
+        this.usuario.nombre = respuesta.nombre;
+        this.usuario.apellidos = respuesta.apellidos;
+        this.usuario.avatar = respuesta.avatar;
+
+        localStorage.setItem('usuario', JSON.stringify(this.usuario));
+        this.edicionPerfil = true;
+        this.enviandoDatosPerfil = false;
+        this.nuevoPerfil.cambiarPassword = false;
+        this.nuevoPerfil.passwordAnterior = '';
+        this.nuevoPerfil.passwordNuevo = '';
+        this.nuevoPerfil.passwordNuevoConfirmacion = '';
+        this.mostrarMensajeExitoso = true;
+
+      }, error => {
+        try {
+          let e = error.json();
+       
+          switch(error.status){
+            case 401: 
+              this.mensajeError =  "No tiee permiso para realizar esta acción.";
+              break;
+            case 409:
+              this.mensajeError = "Verifique la información marcada de color rojo";
+              for (var input in e.error){
+                // Iteramos todos los errores
+                for (var i in e.error[input]){
+                  this.errores[input] = e.error[input][i];
+                }                      
+              }
+              break;
+            case 500:
+              this.mensajeError = "500 (Error interno del servidor)";
+              break;
+            default: 
+              this.mensajeError = "No se puede interpretar el error. Por favor contacte con soporte técnico si esto vuelve a ocurrir.";
+          }
+        } catch (e){
+          this.mensajeError = "No se puede interpretar el error. Por favor contacte con soporte técnico si esto vuelve a ocurrir.";
+        }
+        
+        this.enviandoDatosPerfil = false;
+      }
+    )
+    
   }
 }
--- src/app/perfil/perfil.module.ts
+++ src/app/perfil/perfil.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { PerfilComponent } from './perfil.component';
 import { CambiarEntornoService } from './cambiar-entorno.service';
+import { EditarPerfilService } from './editar-perfil.service';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   exports: [
       PerfilComponent
   ],
-  providers: [CambiarEntornoService],
+  providers: [CambiarEntornoService, EditarPerfilService],
   declarations: [PerfilComponent]
 
 })
