703eb7b34cc34dfe52f0f28fc53d0d68abd0dc38
akira.redwolf@gmail.com <akira.redwolf@gmail.com>
2018-01-20 17:42:19 -0600

    Se corrige la url de la api para las offline

--- src/environments/environment.offline.ts
+++ src/environments/environment.offline.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: "http://api.sial.demo.saludchiapas.gob.mx",
-    RESET_URL: "http://api.sial.demo.saludchiapas.gob.mx/#/login?reset_password=true"
+    API_URL: "api",
+    RESET_URL: ""
   };
