f509304a2fdc41641fb47e309545bba81b352832
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2018-04-05 03:26:22 -0500

    correciones para generar parche

--- src/app/patches.ts
+++ src/app/patches.ts
@@ -1,4 +1,5 @@
 export const PATCHES = [
+	{ nombre:"E62D05D77EAE3119542356AEC2E1F633.sial.cliente.2.patch",fecha:"2018-03-04" }, //listo para generar parche 2
 	{ nombre:"B924B4B668FC43CDCD979783C5BD2699.sial.cliente.1.patch",fecha:"2018-03-04" }
 ];
   
